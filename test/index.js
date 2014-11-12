8bcd0d8587767d4c20b9e6dc5d4fe895f8f63729
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-12 12:14:29 -0600

    implement start of a basic streaming converter very incomplete - returns invalid geojson

--- test/index.js
+++ test/index.js
@@ -1,20 +1,24 @@
 'use strict';
 var fs = require('fs');
 var path = require('path');
+
 var test = require('tape');
+var through = require('through');
 var parser = require('weather-alerts-parser');
 
-var converter = require('../index.js');
+var converter = require('../lib/index.js')();
 
-test('polygon', function (t) {
-  t.plan(1);
+test('single', function (t) {
+  t.plan(2);
 
   var filepath = path.join(__dirname, 'files/single.xml');
   var readable = fs.createReadStream(filepath);
 
-  readable.pipe(parser).pipe(converter);
+  var stream = readable.pipe(parser.stream()).pipe(converter);
 
-  parser.parse(readable, function (error, parsed) {
-    t.equals(parsed.length, 39);
-  });
+  stream.pipe(through(function (fc) {
+    t.equals(fc.type, "FeatureCollection");
+    t.equals(fc.features.length, 8);
+  }));
+
 });
