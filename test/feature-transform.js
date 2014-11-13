463a4be7d71c8af15bc08118a0327f7a929515a7
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-12 21:05:52 -0600

    pull geojson features into a test fixtures module for reuse

--- test/feature-transform.js
+++ test/feature-transform.js
@@ -3,6 +3,7 @@ var geojsonhint = require('geojsonhint');
 var test = require('tape');
 
 var featureTransform = require('../lib/feature-transform.js');
+var fixtures = require('./fixtures');
 
 test('features.polygonize', function (t) {
   var properties = {
@@ -48,115 +49,9 @@ test('features.polygonize', function (t) {
 
 
 test('features.decodify', function (t) {
-  var test1 = {
-    "id": "test1",
-    "type": "Feature",
-    "properties": {
-      "size": 5000
-    },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        [
-          [
-            -108.28125,
-            38.20365531807149
-          ],
-          [
-            -105.99609375,
-            34.30714385628804
-          ],
-          [
-            -101.77734374999999,
-            37.23032838760387
-          ],
-          [
-            -108.28125,
-            38.20365531807149
-          ]
-        ]
-      ]
-    }
-  };
 
-  var test2 = {
-    "type": "Feature",
-    "id": "test2",
-    "properties": {
-      "size": 200
-    },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        [
-          [
-            -98.4375,
-            35.817813158696616
-          ],
-          [
-            -97.6025390625,
-            33.797408767572485
-          ],
-          [
-            -94.1748046875,
-            32.99023555965106
-          ],
-          [
-            -90.7470703125,
-            37.37015718405753
-          ],
-          [
-            -94.9658203125,
-            38.06539235133249
-          ],
-          [
-            -98.4375,
-            35.817813158696616
-          ]
-        ]
-      ]
-    }
-  };
-
-  var testFeature3 = {
-    "type": "Feature",
-    "id": "test3",
-    "properties": {
-      "size": 300
-    },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        [
-          [
-            -100.45898437499999,
-            43.96119063892024
-          ],
-          [
-            -89.2529296875,
-            38.376115424036016
-          ],
-          [
-            -94.9658203125,
-            44.902577996288876
-          ],
-          [
-            -97.03125,
-            45.336701909968106
-          ],
-          [
-            -100.45898437499999,
-            43.96119063892024
-          ]
-        ]
-      ]
-    }
-  };
-
-  var featureCollection = {
-    "type": "FeatureCollection",
-    "features": [test1, test2]
-  };
+  var featureCollection = fixtures.featureCollection;
+  var test2 = fixtures.features[1];
 
   var properties = {
     'name': 'test alert',
@@ -168,7 +63,7 @@ test('features.decodify', function (t) {
   };
 
 
-  var decoded = featureTransform.decodify(featureCollection, 'test2', properties);
+  var decoded = featureTransform.decodify(featureCollection, 'feature2', properties);
 
   t.test('decoded feature properties match passed properties', function (t2) {
     t2.plan(1);
