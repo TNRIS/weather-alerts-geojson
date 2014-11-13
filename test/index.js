8222dacde5ca26b9454c7a5cff2209a5d3f7be73
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-12 18:41:26 -0600

    CLN: put stream constructer in module.exports.stream and clean up test references to it

--- test/index.js
+++ test/index.js
@@ -10,34 +10,34 @@ var parser = require('weather-alerts-parser');
 
 var find = require('lodash.find');
 
-var converter = require('../lib/index.js');
+var geojson = require('../lib/index.js');
 
 test('single.xml', function (t) {
   var filepath = path.join(__dirname, 'files/single.xml');
-  var geojsonStream = converter();
-  var stream = fs.createReadStream(filepath).pipe(parser.stream()).pipe(geojsonStream);
-
   var countyTopo = require('../data/us-counties-10m-topo');
   var countyFeatureCollection = topojson.feature(countyTopo, countyTopo.objects.counties);
 
-  stream.pipe(through(function (fc) {
-    t.test('valid geojson', function(t2) {
-      t2.plan(1);
-      var errors = geojsonhint.hint(JSON.stringify(fc));
-      t2.deepEqual(errors, []);
-    });
+  fs.createReadStream(filepath)
+    .pipe(parser.stream())
+    .pipe(geojson.stream())
+    .pipe(through(function (fc) {
+      t.test('valid geojson', function(t2) {
+        t2.plan(1);
+        var errors = geojsonhint.hint(JSON.stringify(fc));
+        t2.deepEqual(errors, []);
+      });
 
-    t.test('county features are populated', function(t2) {
-      t2.plan(3);
-      t2.equals(fc.features.length, 8);
+      t.test('county features are populated', function(t2) {
+        t2.plan(3);
+        t2.equals(fc.features.length, 8);
 
-      var expectedCounty = find(countyFeatureCollection.features, {id: '048025'});
-      var foundCounty = fc.features[1];
+        var expectedCounty = find(countyFeatureCollection.features, {id: '048025'});
+        var foundCounty = fc.features[1];
 
-      t2.notEqual(expectedCounty.geometry, undefined);
-      t2.deepEqual(expectedCounty.geometry, foundCounty.geometry);
-    });
-  }));
+        t2.notEqual(expectedCounty.geometry, undefined);
+        t2.deepEqual(expectedCounty.geometry, foundCounty.geometry);
+      });
+    }));
 });
 
 
@@ -48,7 +48,7 @@ test('stylize option', function (t) {
     t2.plan(1);
 
     fs.createReadStream(filepath).pipe(parser.stream())
-      .pipe(converter({'stylize': true}))
+      .pipe(geojson.stream({'stylize': true}))
       .pipe(through(function (fc) {
         var feature = fc.features[0];
         t2.equal(feature.properties.fill, '#2E8B57');
@@ -59,7 +59,7 @@ test('stylize option', function (t) {
     t2.plan(1);
 
     fs.createReadStream(filepath).pipe(parser.stream())
-      .pipe(converter({'stylize': false}))
+      .pipe(geojson.stream({'stylize': false}))
       .pipe(through(function (fc) {
         var feature = fc.features[0];
         t2.equal(feature.properties.fill, undefined);
