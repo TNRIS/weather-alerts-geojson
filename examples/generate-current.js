ad44b535ace83cfee996fb1c22ce1cb214257643
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-14 09:55:20 -0600

    drop geocode, parameter and polygon properties when generating current geojohnson

--- examples/generate-current.js
+++ examples/generate-current.js
@@ -8,9 +8,28 @@ var through = require('through');
 var geojson = require('../lib/index.js');
 
 
+function cleanProperties() {
+  var remove = [
+    'geocode',
+    'parameter',
+    'polygon'
+  ];
+
+  return through(function write(featureCollection) {
+    featureCollection.features.forEach(function(feature) {
+      remove.forEach(function(remove) {
+        delete feature.properties[remove];
+      });
+    });
+
+    this.queue(featureCollection);
+  });
+}
+
 request.get('http://alerts.weather.gov/cap/us.php?x=1')
   .pipe(parser.stream())
   .pipe(geojson.stream({'stylize': true}))
+  .pipe(cleanProperties())
   .pipe(geojson.collect({'sort': true, 'flatten': true}))
   .pipe(through(function (obj) {this.queue(JSON.stringify(obj));}))
   .pipe(fs.createWriteStream('current-nws-alerts.json'));
